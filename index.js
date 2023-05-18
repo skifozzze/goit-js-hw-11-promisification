6fe7f3c78eaa6867f673fc7685e32f7ac03f11ff
skifozzze <skifozzze@gmail.com>
2023-05-18 11:42:24 +0300

    upd

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const users = [
-  { name: 'Mango', active: true },
-  { name: 'Poly', active: false },
-  { name: 'Ajax', active: true },
-  { name: 'Lux', active: false },
-];
+// task1
+const delay = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
+};
 
-const toggleUserState = (allUsers, userName) =>
-  Promise.resolve(
-    allUsers.map(user =>
-      user.name === userName ? { ...user, active: !user.active } : user
-    )
-  );
-const logger = updatedUsers => console.table(updatedUsers);
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-toggleUserState(users, 'Mango').then(logger);
-toggleUserState(users, 'Lux').then(logger);
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
+
+// // task2
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
+
+// const toggleUserState = (allUsers, userName) =>
+//   Promise.resolve(
+//     allUsers.map(user =>
+//       user.name === userName ? { ...user, active: !user.active } : user
+//     )
+//   );
+// const logger = updatedUsers => console.table(updatedUsers);
+
+// toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Lux').then(logger);
