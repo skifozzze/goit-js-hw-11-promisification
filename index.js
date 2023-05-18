23a469a59cce195d03428267a862b784bd3f40ca
skifozzze <skifozzze@gmail.com>
2023-05-18 11:33:14 +0300

    task2

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// task1
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
 
-const delay = ms => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      logger(ms);
-    }, ms);
-  });
-};
+const toggleUserState = (allUsers, userName) =>
+  Promise.resolve(
+    allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    )
+  );
+const logger = updatedUsers => console.table(updatedUsers);
 
-const logger = time => console.log(`Resolved after ${time} ms`);
-
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
